d043640cdba1708ee7a4a76e08140c380b32cd9d
Carl Andersson <git@lillecarl.com>
2025-10-29 01:43:51 +0100

    Patch Lix to update registrationTime on path query
    This is the cornerstone to keeping paths around on the CSI nodes and on
    the cache without fucking with gcroots or running privileged commands
    afterwards and such. Just repurpose registrationTime for collecting
    garbage since it's not used for anything else in Nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -22,6 +22,17 @@ self: pkgs: {
     inherit (self) csi-proto-python kr8s;
   };
 
+  lix = pkgs.lix.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [
+      (pkgs.fetchpatch {
+        url = "https://github.com/Lillecarl/lix/commit/b5e5a56b93da57239feb192416291af21df8dfe8.patch";
+        hash = "sha256-b65/gMXdC1oghF5SPdmYYeqkwifzCW/cb6TGbwkne8U=";
+      })
+    ];
+    doCheck = false;
+    doInstallCheck = false;
+  });
+
   csi-proto-python = pkgs.python3Packages.callPackage ./csi-proto-python { };
   python-jsonpath = pkgs.python3Packages.callPackage ./python-jsonpath.nix { };
   kr8s = pkgs.python3Packages.callPackage ./kr8s.nix { inherit (self) python-jsonpath; };
