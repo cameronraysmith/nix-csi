a15bad7d06ed89c9e4a3015c5f79535daef4f244
Carl Andersson <git@lillecarl.com>
2025-11-12 21:03:54 +0100

    Remove kubenix flake input

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    kubenix = {
-      url = "github:hall/kubenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = inputs: { };
 }
