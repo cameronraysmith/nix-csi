9de0565d8a2b6e5996cd557a756001d03d2b0b01
Carl Andersson <git@lillecarl.com>
2025-11-24 07:05:16 +0100

    remove flake support
    flakes are so fucking dumb, can't read from FS to make some convenience
    methods, no everything must be tracked by Git and imported into store.

--- flake.nix
+++ flake.nix
@@ -18,35 +18,5 @@
       flake = false;
     };
   };
-  outputs =
-    inputs:
-    let
-      eachSystem = inputs.nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-    in
-    {
-      packages = eachSystem (
-        system:
-        let
-          pkgs = import inputs.nixpkgs { inherit system; };
-          nix-csi = import ./default.nix { inherit pkgs; };
-        in
-        {
-          image = nix-csi.image;
-          inherit
-            (nix-csi.easykubenix {
-              modules = [
-                ./kubenix
-                { nix-csi.enable = true; }
-              ];
-            })
-            deploymentScript
-            manifestYAMLFile
-            manifestJSONFile
-            ;
-        }
-      );
-    };
+  outputs = inputs: { };
 }
